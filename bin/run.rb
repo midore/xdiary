b657e5d140760290bb60977d27cb74dba76a984b
midore <midorex@gmail.com>
2012-10-12 22:50:10 +0900

    change setting path to conf

--- bin/run.rb
+++ bin/run.rb
@@ -10,17 +10,17 @@ module Xdiary
   class Start
     def initialize
       $LOAD_PATH.delete(".")
-      dir = File.dirname(File.dirname(File.expand_path($PROGRAM_NAME)))
-      $LOAD_PATH.push(File.join(dir, 'lib'))
-
+      # Setting path to 'xdiary-conf'
+      # dir = File.dirname(File.dirname(File.expand_path($PROGRAM_NAME)))
+      # $LOAD_PATH.push(File.join(dir, 'lib'))
+      #
       # [Default setting]
-      conf_path = File.join(dir, 'xdiary-conf')
-      # It's means setting like '/path/to/xdiary/xdiary-conf'
+      # conf_path = File.join(dir, 'xdiary-conf')
       #
       # [Customize setting]
       # conf_path = '/path/to/yours/xdiary-conf'
       #
-
+      #
       (print "Error: not found xdiary-conf\n"; exit) unless File.exist?(conf_path)
       load conf_path, wrap=true
     end
